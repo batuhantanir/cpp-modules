80a7a7113f02963ad70595dd20fad7fe0a293cff
Batuhan TANIR <batuhan.tanir00@gmail.com>
2024-08-12 11:59:10 +0300

    created cpp00 ex00

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+    if (argc > 1)
+    {
+        for (int i = 1; i < argc; i++)
+        {
+            size_t len = strlen(argv[i]);
+            for (size_t j = 0; j < len; j++)
+                std::cout << (char)toupper(argv[i][j]);
+        }
+    }
+    else
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+    std::cout << std::endl;
+    return 0;
+}
